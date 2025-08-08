119e0dab288de0f054d8b2086b17d75d6f68319d
wallaceholanda <wallace.holanda@hotmail.com>
2025-08-08 16:54:14 -0300

    feat: add drawer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,55 @@
-import { Stack } from 'expo-router/stack';
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
-    return <Stack>
-        <Stack.Screen
-            name='pomodoro'
-            options={{ headerShown: false }}
-        />
-        <Stack.Screen
-            name='index'
-            options={{ headerShown: false }}
-        />
-    </Stack>
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer
+                screenOptions={{
+                    headerTintColor: '#FFF',
+                    headerStyle: {
+                        backgroundColor: '#021123',
+                    },
+                    drawerStyle: {
+                        backgroundColor: '#021123',
+                    },
+                    drawerLabelStyle: {
+                        color: '#FFF',
+                    }
+                }}
+            >
+                <Drawer.Screen
+                    name='index'
+                    options={{
+                        headerShown: false,
+                        drawerItemStyle: {
+                            display: 'none',
+                        }
+                    }}
+                />
+                <Drawer.Screen
+                    name='add-task/index'
+                    options={{
+                        drawerItemStyle: {
+                            display: 'none',
+                        }
+                    }}
+                />
+                <Drawer.Screen
+                    name='pomodoro'
+                    options={{
+                        title: '',
+                        drawerLabel: 'Timer',
+                    }}
+                />
+                <Drawer.Screen
+                    name='tasks/index'
+                    options={{
+                        title: '',
+                        drawerLabel: 'TLista de Tarefas',
+                    }}
+                />
+            </Drawer>
+        </GestureHandlerRootView>
+    );
 }
---
+++ app/add-task/index.jsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function AddTask() {
+    return (
+        <SafeAreaView>
+            <Text>Add Task Page</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
