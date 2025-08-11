282ffe19b77a5aa5b12ff0e4f37eef91b4bc5791
wallaceholanda <wallace.holanda@hotmail.com>
2025-08-11 17:02:31 -0300

    feat: create task provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,65 +2,68 @@ import { Ionicons } from '@expo/vector-icons';
 import { router } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { TasksProvider } from '../components/context/TaskProvider';
 
 export default function Layout() {
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer
-                screenOptions={{
-                    headerTintColor: '#FFF',
-                    headerStyle: {
-                        backgroundColor: '#021123',
-                    },
-                    drawerStyle: {
-                        backgroundColor: '#021123',
-                    },
-                    drawerLabelStyle: {
-                        color: '#FFF',
-                    }
-                }}
-            >
-                <Drawer.Screen
-                    name='index'
-                    options={{
-                        headerShown: false,
-                        drawerItemStyle: {
-                            display: 'none',
+        <TasksProvider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <Drawer
+                    screenOptions={{
+                        headerTintColor: '#FFF',
+                        headerStyle: {
+                            backgroundColor: '#021123',
+                        },
+                        drawerStyle: {
+                            backgroundColor: '#021123',
+                        },
+                        drawerLabelStyle: {
+                            color: '#FFF',
                         }
                     }}
-                />
-                <Drawer.Screen
-                    name='add-task/index'
-                    options={{
-                        drawerItemStyle: { display: 'none' },
-                        title: '',
-                        headerLeft: () => {
-                            return <Ionicons
-                                name='arrow-back'
-                                size={24}
-                                color='#FFF'
-                                style={{ marginLeft: 16 }}
-                                onPress={() => router.navigate('/tasks')}
-                            />
-                        },
+                >
+                    <Drawer.Screen
+                        name='index'
+                        options={{
+                            headerShown: false,
+                            drawerItemStyle: {
+                                display: 'none',
+                            }
+                        }}
+                    />
+                    <Drawer.Screen
+                        name='add-task/index'
+                        options={{
+                            drawerItemStyle: { display: 'none' },
+                            title: '',
+                            headerLeft: () => {
+                                return <Ionicons
+                                    name='arrow-back'
+                                    size={24}
+                                    color='#FFF'
+                                    style={{ marginLeft: 16 }}
+                                    onPress={() => router.navigate('/tasks')}
+                                />
+                            },
 
-                    }}
-                />
-                <Drawer.Screen
-                    name='pomodoro'
-                    options={{
-                        title: '',
-                        drawerLabel: 'Timer',
-                    }}
-                />
-                <Drawer.Screen
-                    name='tasks/index'
-                    options={{
-                        title: '',
-                        drawerLabel: 'Lista de Tarefas',
-                    }}
-                />
-            </Drawer>
-        </GestureHandlerRootView>
+                        }}
+                    />
+                    <Drawer.Screen
+                        name='pomodoro'
+                        options={{
+                            title: '',
+                            drawerLabel: 'Timer',
+                        }}
+                    />
+                    <Drawer.Screen
+                        name='tasks/index'
+                        options={{
+                            title: '',
+                            drawerLabel: 'Lista de Tarefas',
+                        }}
+                    />
+                </Drawer>
+            </GestureHandlerRootView>
+        </TasksProvider>
     );
 }
---
+++ components/context/TaskProvider.jsx
@@ -0,0 +1,48 @@
+import { createContext } from "react";
+
+export const TaskContext = createContext();
+
+export function TasksProvider(children) {
+
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (description) => {
+        setTasks((prevTasks) => {
+            return [
+                ...prevTasks,
+                {
+                    description,
+                    id: prevTasks.length + 1,
+                },
+            ]
+        })
+    }
+
+    const toggleTaskCompletion = (id) => {
+        setTasks((prevTasks) => {
+            return prevTasks.map(task => {
+                if (task.id === id) {
+                    return { ...task, completed: !task.completed };
+                }
+                return task;
+            });
+        });
+    }
+
+    const deleteTask = (id) => {
+        setTasks((prevTasks) => {
+            return prevTasks.filter(task => task.id !== id);
+        });
+    }
+
+    return (
+        <TaskContext.Provider value={{
+            tasks,
+            addTask,
+            deleteTask,
+            toggleTaskCompletion,
+        }}>
+            {children}
+        </TaskContext.Provider>
+    );
+}
