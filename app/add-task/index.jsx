768e89e4c2bc14e102e22cd4acfc87f41ad2a10e
wallaceholanda <wallace.holanda@hotmail.com>
2025-08-13 14:43:58 -0300

    feat: create form task component

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,102 +1,17 @@
 import { router } from "expo-router";
-import { useState } from "react";
-import { Keyboard, KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
 import useTaskContext from "../../components/context/useTaskContext";
-import { IconSave } from "../../components/icons";
+import { FormTask } from "../../components/FormTask";
 export default function AddTask() {
-
-    const [description, setDescription] = useState();
     const { addTask } = useTaskContext();
 
-    const submitTask = () => {
-        if (!description) {
-            return;
-        }
+    const submitTask = (description) => {
         addTask(description);
-        setDescription('');
         router.navigate('/tasks');
     }
 
     return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        >
-            <TouchableWithoutFeedback
-                onPress={Keyboard.dismiss}
-            >
-                <View style={styles.inner}>
-                    <Text style={styles.title}>
-                        Adicionar uma tarefa:
-                    </Text>
-                    <Text style={styles.formTitle}>
-                        Em que você está trabalhando?
-                    </Text>
-                    <TextInput
-                        multiline={true}
-                        numberOfLines={10}
-                        style={styles.formInput}
-                        value={description}
-                        onChangeText={setDescription}
-                    />
-                    <Pressable
-                        style={styles.saveButton}
-                        onPress={() => {
-                            submitTask();
-                        }}
-                    >
-                        <IconSave />
-                        <Text style={styles.buttonText}>
-                            Salvar
-                        </Text>
-
-                    </Pressable>
-                </View>
-            </TouchableWithoutFeedback>
-        </KeyboardAvoidingView >
+        <FormTask
+            onFormSubmit={submitTask}
+        />
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        gap: 16,
-        backgroundColor: '#021123',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 26,
-        color: '#FFF',
-        textAlign: 'center',
-    },
-    inner: {
-        gap: 32,
-        padding: 16,
-        width: '90%',
-        borderRadius: 8,
-        backgroundColor: '#98A0A8',
-    },
-    formTitle: {
-        color: '#000',
-        fontSize: 18,
-        textAlign: 'left',
-        fontWeight: '600',
-    },
-    formInput: {
-        padding: 16,
-        height: 100,
-        borderRadius: 8,
-        backgroundColor: '#FFF',
-    },
-    saveButton: {
-        gap: 4,
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-    },
-    buttonText: {
-        fontSize: 18,
-        color: '#000',
-        fontWeight: 'bold',
-    },
-});
---
+++ components/FormTask/index.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
+import { IconSave } from "../icons";
+
+
+export const FormTask = ({
+    defaultValue = '',
+    onFormSubmit,
+}) => {
+
+    const [description, setDescription] = useState(defaultValue);
+
+    const submitTask = () => {
+        if (!description) {
+            return;
+        }
+        onFormSubmit(description);
+        setDescription('');
+    }
+
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <TouchableWithoutFeedback
+                onPress={Keyboard.dismiss}
+            >
+                <View style={styles.inner}>
+                    <Text style={styles.title}>
+                        {defaultValue ? 'Editar' : 'Adicionar'} uma tarefa:
+                    </Text>
+                    <Text style={styles.formTitle}>
+                        Em que você está trabalhando?
+                    </Text>
+                    <TextInput
+                        multiline={true}
+                        numberOfLines={10}
+                        value={description}
+                        style={styles.formInput}
+                        onChangeText={setDescription}
+                    />
+                    <Pressable
+                        style={styles.saveButton}
+                        onPress={() => {
+                            submitTask();
+                        }}
+                    >
+                        <IconSave />
+                        <Text style={styles.buttonText}>Salvar</Text>
+                    </Pressable>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 16,
+        alignItems: 'center',
+        backgroundColor: '#021123',
+    },
+    title: {
+        fontSize: 26,
+        color: '#FFF',
+        textAlign: 'center',
+    },
+    inner: {
+        gap: 32,
+        padding: 16,
+        width: '90%',
+        borderRadius: 8,
+        backgroundColor: '#98A0A8',
+    },
+    formTitle: {
+        color: '#000',
+        fontSize: 18,
+        textAlign: 'left',
+        fontWeight: '600',
+    },
+    formInput: {
+        padding: 16,
+        height: 100,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+    },
+    saveButton: {
+        gap: 4,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#000',
+        fontWeight: 'bold',
+    },
+});
